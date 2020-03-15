5e8dc53c0b7d07ea18ada7f3cc1235daf802193e
Nada8773 <nada2478@gmail.com>
2020-03-16 01:55:47 +0200

    Add GUI Calculator Project

---
+++ GUI Calculator/Calculator.m
@@ -0,0 +1,195 @@
+function varargout = Calculator(varargin)
+% CALCULATOR MATLAB code for Calculator.fig
+%      CALCULATOR, by itself, creates a new CALCULATOR or raises the existing
+%      singleton*.
+%
+%      H = CALCULATOR returns the handle to a new CALCULATOR or the handle to
+%      the existing singleton*.
+%
+%      CALCULATOR('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CALCULATOR.M with the given input arguments.
+%
+%      CALCULATOR('Property','Value',...) creates a new CALCULATOR or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Calculator_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Calculator_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Calculator
+
+% Last Modified by GUIDE v2.5 31-Dec-2019 20:31:22
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Calculator_OpeningFcn, ...
+                   'gui_OutputFcn',  @Calculator_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Calculator is made visible.
+function Calculator_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Calculator (see VARARGIN)
+
+% Choose default command line output for Calculator
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Calculator wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Calculator_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function num1_Callback(hObject, eventdata, handles)
+% hObject    handle to num1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of num1 as text
+%        str2double(get(hObject,'String')) returns contents of num1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function num1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to num1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function num2_Callback(hObject, eventdata, handles)
+% hObject    handle to num2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of num2 as text
+%        str2double(get(hObject,'String')) returns contents of num2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function num2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to num2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in choice.
+function choice_Callback(hObject, eventdata, handles)
+% hObject    handle to choice (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns choice contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from choice
+
+
+% --- Executes during object creation, after setting all properties.
+function choice_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to choice (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function result_Callback(hObject, eventdata, handles)
+% hObject    handle to result (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of result as text
+%        str2double(get(hObject,'String')) returns contents of result as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function result_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to result (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+ first_num=str2double(get(handles.num1,'string'));
+ second_num=str2double(get(handles.num2,'string'));
+ content=get(handles.choice,'String');
+ choice_operation=content{get(handles.choice,'Value')};
+ 
+ switch choice_operation
+    case '+'
+        sum=first_num+second_num;
+    case '-'
+        sum=first_num-second_num;
+    case '*'
+        sum=first_num*second_num;
+    case '/'
+        sum=first_num/second_num;
+ end
+ if(sum>=100)
+     set(handles.result,'string',num2str(sum));
+     set(handles.result,'BackgroundColor','green');
+     image1=imread('im1.jpg');
+     imshow(image1)
+ else
+     set(handles.result,'string',num2str(sum));
+     set(handles.result,'BackgroundColor','red');
+     image2=imread('im2.png');
+     imshow(image2)
+ end
